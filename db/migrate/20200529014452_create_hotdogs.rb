6ccdd673de27bcd2b077b42688d40f529f827e83
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-05-29 13:48:17 +1200

    ARCH: Added models and database

---
+++ db/migrate/20200529014452_create_hotdogs.rb
@@ -0,0 +1,10 @@
+class CreateHotdogs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :hotdogs do |t|
+      t.string :name
+      t.string :comment
+
+      t.timestamps
+    end
+  end
+end
